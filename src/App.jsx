3fd135cac1d24d973ecb7d2a5714b45feedfc87a
th3N0m4d <edielton.dantas@hotmail.com>
2020-01-10 10:21:01 +0100

    Add support to Enzyme

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1>App component</h1>
+    <p>Hello world!</p>
+  </div>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import './App.scss'
-
-const App = () => (
-  <div>
-    <span className='lnr lnr-clock' /> lnr-clock
-  </div>
-)
+import App from './App'
 
 render(
   <App />,
